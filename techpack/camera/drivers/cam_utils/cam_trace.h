8669980c77e6ad30a689d476e3ab0311b5e0c758
wengu0506 <246553774@qq.com>
2026-03-12 10:22:18 +0800

    Update cam_trace.h

--- techpack/camera/drivers/cam_utils/cam_trace.h
+++ techpack/camera/drivers/cam_utils/cam_trace.h
@@ -15,7 +15,7 @@
 
 #include <linux/tracepoint.h>
 #include <media/cam_req_mgr.h>
-#include "cam_req_mgr_core.h"
+#include "../cam_req_mgr/cam_req_mgr_core.h"
 #include "cam_req_mgr_interface.h"
 #include "cam_context.h"
 
